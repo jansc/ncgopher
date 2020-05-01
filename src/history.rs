ae0938a3e1686dc4fcbfccfe5a3dab484552a563
Jan Schreiber <jans@ravn.no>
2020-05-01 14:39:10 +0200

    Store history in a SQLite database

--- src/history.rs
+++ src/history.rs
@@ -1,13 +1,8 @@
 use chrono::{DateTime, Local};
-use config::{Config, File, FileFormat};
 use serde::Serializer;
-use std::cmp;
-use std::fs::File as FsFile;
-use std::io::Write;
-use std::path::Path;
 use std::sync::Arc;
 use url::Url;
-use rusqlite::{Connection, NO_PARAMS};
+use rusqlite::{Connection, NO_PARAMS, params};
 
 fn url_serialize<S>(url: &Url, s: S) -> Result<S::Ok, S::Error>
 where
@@ -42,12 +37,12 @@ pub struct History {
     /// Navigational stack, used for back-functionality
     pub stack: Vec<HistoryEntry>,
     /// Log of all visited gopherholes
-    pub entries: Vec<HistoryEntry>,
     sql: Arc<Connection>,
 }
 
 impl History {
     pub fn new() -> History {
+        /*
         let mut s = Config::new();
         let confdir = History::get_history_filename();
         if Path::new(confdir.as_str()).exists() {
@@ -58,8 +53,9 @@ impl History {
                 }
             }
         }
-        let mut entries = Vec::new();
-        let connection = Arc::new(Connection::open_in_memory().unwrap());
+        */
+        info!("Creating history object");
+        let connection = Arc::new(Connection::open(History::get_history_filename()).unwrap());
         connection.execute(
             "CREATE TABLE IF NOT EXISTS history (
              id INTEGER PRIMARY KEY,
@@ -70,27 +66,8 @@ impl History {
          )",
             NO_PARAMS,
         ).expect("Could not create history table");
-        info!("Reading history...");
-        info!("history: {:?}", s.get_array("history"));
-        if let Ok(e) = s.get_array("history") {
-            for value in e {
-                if let Ok(v) = value.into_table() {
-                    if let Ok(u) = Url::parse(v["url"].clone().into_str().unwrap().as_str()) {
-                        let h = HistoryEntry {
-                            url: u.clone(),
-                            title: v["url"].clone().into_str().unwrap(),
-                            timestamp: Local::now(),
-                            visited_count: 1,
-                            position: 0,
-                        };
-                        entries.push(h.clone());
-                    }
-                }
-            }
-        }
         History {
             stack: Vec::new(),
-            entries,
             sql: connection,
         }
     }
@@ -99,7 +76,7 @@ impl History {
         let confdir: String = match dirs::config_dir() {
             Some(mut dir) => {
                 dir.push(env!("CARGO_PKG_NAME"));
-                dir.push("history");
+                dir.push("history.db");
                 dir.into_os_string().into_string().unwrap()
             }
             None => String::new(),
@@ -111,39 +88,35 @@ impl History {
     pub fn add(&mut self, entry: HistoryEntry) {
         info!("Adding entry to history: {:?}", entry);
         self.stack.push(entry.clone());
-        self.sql.execute(
-            "INSERT INTO history (url) values (?1)",
-            &[&entry.url.to_string()],
-        ).expect("Could not write history entry");
-        // Check if the element already exists in history
-        match self.entries.iter().position(|e| e.url == entry.url) {
-            Some(p) => {
-                let mut item = self.entries.remove(p);
-                info!("removed item {:?}", item);
-                item.visited_count += 1;
-                self.entries.insert(0, item);
-            }
-            None => {
-                self.entries.insert(0, entry);
-            }
-        }
-        match self.write_history_to_file() {
-            Err(why) => warn!("Could not write history file: {}", why),
-            Ok(()) => (),
+
+        trace!("History::add(): checking for entry with url {}", entry.url);
+        if let Ok(_) = self.sql.query_row(
+            "SELECT id FROM history WHERE url=?1",
+            params![&entry.url],
+            |_| Ok(())
+        ) {
+            trace!("History::add(): Row exists, updating");
+            let mut stmt = self.sql.prepare("UPDATE history SET visitedcount=visitedcount+1 WHERE url=?1").expect("Could not update history entry");
+            stmt.query(params![&entry.url.to_string()]);
+        } else {
+            trace!("History::add(): Adding entry");
+            self.sql.execute(
+                "INSERT INTO history (url) values (?1)",
+                &[&entry.url.to_string()],
+            ).expect("Could not write history entry");
+            let last_id: String = self.sql.last_insert_rowid().to_string();
+            info!("History::add(): Last entry id {}", last_id); 
         }
     }
 
     pub fn clear(&mut self) {
+        trace!("History::clear()");
         self.stack.clear();
-        self.entries.clear();
         self.sql.execute("DELETE FROM history", NO_PARAMS).expect("Could not write history entry");
-        match self.write_history_to_file() {
-            Err(why) => warn!("Could not write history file: {}", why),
-            Ok(()) => (),
-        }
     }
 
     pub fn back(&mut self) -> Option<HistoryEntry> {
+        // Removes the topmost entry from the history and returns it
         if self.stack.len() > 1 {
             self.stack.pop();
             let item = self.stack.pop().unwrap();
@@ -169,40 +142,23 @@ impl History {
 
     pub fn get_latest_history(&self, num_items: usize) -> Vec<HistoryEntry> {
         let mut res = Vec::<HistoryEntry>::new();
-        let count = cmp::min(num_items, self.entries.len());
-        for i in 0..count {
-            res.insert(0, self.entries[i].clone());
-        }
+        info!("Getting {} history entries", num_items as u32);
+
+        let mut stmt = self.sql.prepare("SELECT title, url, timestmp, visitedcount FROM history ORDER BY timestmp LIMIT ?1").expect("Could not get history entry");
+        let mut rows = stmt.query(params![num_items as u32]).expect("Could not execute query");
+        while let Some(row) = rows.next().expect("Could not read rows") {
+            info!("Found history entry");
+            let title: String = row.get(1).expect("Could not get title");
+            let entry = HistoryEntry {
+                title: title,
+                url: row.get(1).expect("Could not get row"),
+                timestamp: row.get(2).expect("Could not get row"),
+                visited_count: row.get(3).expect("Could not get row"),
+                position: 0
+            };
+            res.push(entry);
+        };
+        info!("Returning {} history entries", res.len());
         res
-    }
-
-    pub fn write_history_to_file(&mut self) -> std::io::Result<()> {
-        let filename = History::get_history_filename();
-        info!("Saving history to file: {}", filename);
-        // Create a path to the desired file
-        let path = Path::new(&filename);
-
-        let mut file = match FsFile::create(&path) {
-            Err(why) => return Err(why),
-            Ok(file) => file,
-        };
-
-        if let Err(why) = file.write(b"# Automatically generated by ncgopher.\n") {
-            return Err(why);
-        };
-        for h in self.clone().entries {
-            self.sql.execute(
-                "INSERT INTO history (url) values (?1)",
-                &[&h.url.to_string()],
-            ).expect("Could not write history entry");
-            if let Err(why) = file.write(b"\n[[history]]\n") {
-                return Err(why);
-            };
-            let item = toml::to_string(&h).unwrap();
-            if let Err(why) = file.write_all(item.as_bytes()) {
-                return Err(why);
-            };
-        }
-        Ok(())
     }
 }
